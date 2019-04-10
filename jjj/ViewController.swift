ce003adc3d846ea30c4c00bee7d6b3258c07d464
hsumax0216 <b26262235@gmail.com>
2019-04-10 21:30:38 +0800

    qq

--- jjj/ViewController.swift
+++ jjj/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet var datelabel: UILabel!
+   
+    @IBOutlet var datepicker: UIDatePicker!
     @IBOutlet var price: UILabel!
     @IBOutlet var enter: UIButton!
     @IBOutlet var pic: UIImageView!
@@ -18,6 +21,7 @@ class ViewController: UIViewController {
     @IBOutlet var brend: UISegmentedControl!
     @IBOutlet var priceslider: UISlider!
     @IBOutlet var tf: UITextField!
+    @IBOutlet var YGC: UILabel!
     
     
     override func viewDidLoad() {
@@ -39,50 +43,125 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rndswitch(_ sender: UISwitch) {
-        let randomnum=Int.random(in:0...1)
         
         if (sender.isOn) {
-            brend.setEnabled(false, forSegmentAt: 0)
-            brend.setEnabled(false, forSegmentAt: 1)
-            
-            switch(randomnum){
-            case 0:
-                pic.image = UIImage(named: "0")
-                break;
-            case 1:
-                pic.image = UIImage(named: "ubfu0dAD")
-                break;
-            default:
-                break;
-            }
-            
+            priceslider.isEnabled=false
         }
         else{
-            brend.setEnabled(true, forSegmentAt: 0)
-            brend.setEnabled(true, forSegmentAt: 1)
+            priceslider.isEnabled=true
         }
     }
     
     @IBAction func segm(_ sender: Any) {
-        switch(brend.selectedSegmentIndex){
-        case 0:
-            pic.image = UIImage(named: "ubfu0dAD")
-            break;
-        case 1:
-            pic.image = UIImage(named: "0")
-            break;
-        default:
-            break;
-        }
+        
     }
     
     @IBAction func gogo(_ sender: Any) {
+        if let input=price.text,let n=Int(input){
+            switch(brend.selectedSegmentIndex){
+            case 0:
+                whichgraphiccard(b: 0, _num:n)
+                break;
+            case 1:
+                whichgraphiccard(b: 1, _num:n)
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    func whichgraphiccard(b:Int , _num:Int){
         
+            switch(b){
+            case 0:
+                if _num<3800{
+                    YGC.text="no graphic card"
+                    pic.image = UIImage(named: "white")
+                }
+                else if _num<=4800 && _num>=3800{
+                    pic.image = UIImage(named: "570")
+                    YGC.text="Radeon RX 570 4G"
+                }
+                else if _num>4800 && _num<=6800{
+                    pic.image = UIImage(named: "580")
+                    YGC.text="Radeon RX 580 4G"
+                }
+                else if _num>6800 && _num<7800{
+                    pic.image = UIImage(named: "580")
+                    YGC.text="Radeon RX 580 8G"
+                }
+                else if _num>=7800 && _num<23000{
+                    pic.image = UIImage(named: "590")
+                    YGC.text="Radeon RX 590 8G"
+                }
+                else if _num>=23000{
+                    pic.image = UIImage(named: "radeonvii")
+                    YGC.text="Radeon VII"
+                }
+                break
+            case 1:
+                if _num<3000{
+                    YGC.text="no graphic card"
+                    pic.image = UIImage(named: "white")
+                }
+                else if _num>=3000 && _num<4000{
+                    pic.image = UIImage(named: "1050")
+                    YGC.text="Geforce GTX 1050"
+                }
+                else if _num>=4000 && _num<6300{
+                    pic.image = UIImage(named: "10603g")
+                    YGC.text="Geforce GTX 1060 3G"
+                }
+                else if _num<7300 && _num>=6300{
+                    pic.image = UIImage(named: "10606g")
+                    YGC.text="Geforce GTX 1060 6G"
+                }
+                else if _num<9000 && _num>=7300{
+                    pic.image = UIImage(named: "1660")
+                    YGC.text="Geforce GTX 1660"
+                }
+                else if _num<12000 && _num>=9000{
+                    pic.image = UIImage(named: "1660ti")
+                    YGC.text="Geforce GTX 1660 ti"
+                }
+                else if _num<16000 && _num>=12000{
+                    pic.image = UIImage(named: "2060")
+                    YGC.text="Geforce RTX 2060"
+                }
+                else if _num<23000 && _num>=16000{
+                    pic.image = UIImage(named: "2070")
+                    YGC.text="Geforce RTX 2070"
+                }
+                else if _num<37000 && _num>=23000{
+                    pic.image = UIImage(named: "2080")
+                    YGC.text="Geforce RTX 2080"
+                }
+                else if _num>=37000{
+                    pic.image = UIImage(named: "2080ti")
+                    YGC.text="Geforce RTX 2080 ti"
+                }
+                break
+            default:
+                break;
+            }
+            
+            
+        }
+    
+    @IBAction func randomcard(_ sender: Any) {
+        let r1 = Int.random(in: 3800...40000)
+        let r2 = Int.random(in:0...1)
+        whichgraphiccard(b:r2, _num:r1)
     }
-    func whichgraphiccard(){
-        
+    
+    @IBAction func dp(_ sender: Any) {
+        datepicker.datePickerMode = UIDatePicker.Mode.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        let selectedDate = dateFormatter.string(from: datepicker.date)
+        datelabel.text = selectedDate
     }
-    @IBAction func taimu(_ sender: Any) {
-    }
+    
+    
 }
 
